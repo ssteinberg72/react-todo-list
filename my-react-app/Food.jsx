396c71c08ea8e93b892f61716c43a1edda5fee49
ssteinberg72 <sstein18@gmail.com>
2025-06-24 15:06:19 -0400

    ReactJS exercises
    ReactJS ToDo List

---
+++ my-react-app/Food.jsx
@@ -0,0 +1,27 @@
+/*import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'*/
+
+function Food() {
+  /*const [count, setCount] = useState(0)*/
+
+  const food1 = "Hamburgers";
+  const food2 = "Hot Dogs";
+
+  return (
+      <header>
+        <h1>Steve's Food Selections</h1>
+        <div id="felections" border="1px" name="FoodSelections" width="400">
+          <div>
+            {food1}
+            </div>
+            <div>
+            {food2.toUpperCase()}
+          </div>
+        </div>
+        </header>
+  )
+}
+
+export default Food
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,23 @@
+/*import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'*/
+
+function Header() {
+  /*const [count, setCount] = useState(0)*/
+
+  return (
+      <header>
+        <h1>Steve's Awesome Website</h1><nav>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Services</li>
+            <li>Contact Us</li>
+          </ul>
+        </nav>
+        </header>
+  )
+}
+
+export default Header
